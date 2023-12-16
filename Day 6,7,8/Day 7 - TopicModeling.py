99bef9519aadbb066ae54641049c34f3c3b42dc6
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-12-16 21:21:27 +0545

    SVD, NMF, TFIDF theory Stuck with versioning error of python

---
+++ Day 6,7,8/Day 7 - TopicModeling.py
@@ -0,0 +1,15 @@
+'''
+Singular Value Decomposition or SVD: 
+The words that appear most frequently in one topic would appear 
+less frequently in the other, otherwise that word wouldn't make a good choice to separate out the two topics. 
+Therefore, the Topics are Orthogonal. The SVD algorithm factorizes a matrix into one matrix with orthogonal 
+columns and one with orthogonal rows along with diagonal matrix which contains the relative importance of each factor.
+
+NonNegative Matrix Factorization or NMF: 
+Non Negative Matrix Factorization (NMF) is a factorization or constrain of non negative dataset. NMF is non exact 
+factorization that factors into one short positive matrix.
+
+Topic Frequency Inverse Document Frequency or TFIDF: 
+TFIDF is a way to normalize the term counts by taking into account how often they appear in a document and how long the 
+document is and how common or rare the document is.
+'''
