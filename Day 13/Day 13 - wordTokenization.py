958cebf51428317b499ebae67a4506f9e18797ec
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-12-22 10:12:51 +0545

    Word Tokenization (More Preprocessing)

--- Day 13/Day 13 - wordTokenization.py
+++ Day 13/Day 13 - wordTokenization.py
@@ -53,3 +53,58 @@ DataFrame for Multiple Sentences: The text extends the example to include multip
 showcasing how the DataFrame can be used as a corpus representation.
 
 '''
+import numpy as np
+import pandas as pd
+import re
+import nltk
+from nltk.tokenize import RegexpTokenizer
+
+sentence = """"Hello everyone, I am Sandesh Jung Kunwar \
+and I am learning Natural Language Processing.""" 
+
+token_sequence = str.split(sentence) #str.split() quick and dirty tokenizer
+vocab = sorted(set(token_sequence)) # Unique tokens in the sentence
+# ', '.join(vocab)
+num_tokens = len(token_sequence) # Number of tokens
+vocab_size = len(vocab) # Number of Unique tokens
+# num_tokens, vocab_size
+onehot_vectors = np.zeros((num_tokens, vocab_size), int) 
+for i, word in enumerate(token_sequence):
+    onehot_vectors[i, vocab.index(word)] = 1
+# ' '.join(vocab)
+    # onehot_vectors
+
+'''
+A DataFrame keeps track of labels for each column, allowing you to label each column in our table with the 
+token or word it represents. A DataFrame can also keep
+track of labels for each row in the DataFrame.index, for speedy lookup.
+'''
+# pd.DataFrame(onehot_vectors, columns=vocab)
+
+# df = pd.DataFrame(pd.Series(dict([(token, 1) for token in sentence.split()])),columns=['sent']).T
+# df
+corpus = {}
+for i, sent in enumerate(sentence.split('\n')):
+    corpus["sent{}".format(i)] = dict((tok, 1) for tok in sent.split())
+df = pd.DataFrame.from_records(corpus).fillna(0).astype(int).T
+pattern = re.compile(r"([-\s.,?!:;])+")
+tokens = pattern.split(sentence)
+tokens
+list(filter(lambda x:x if x and x not in "- \t\n.,?!" else None, tokens)) #Implementation of lambda
+
+#@ Implementation of Regexp Tokenizer
+tokenizer = RegexpTokenizer(r'\w+|$[0-9.]+|\S+')
+tokenizer.tokenize(sentence)
+
+#@ Implementation of Treebank Tokenizer
+from nltk.tokenize import TreebankWordTokenizer
+tokenizer = TreebankWordTokenizer()
+tokenizer.tokenize(sentence)
+
+#@ Implementation of Casual TOkenizer
+from nltk.tokenize.casual import casual_tokenize
+casual_tokenize(sentence)
+
+#@ Implementation of NGram Tokenizer
+from nltk.util import ngrams
+list(ngrams(tokens, 3))
