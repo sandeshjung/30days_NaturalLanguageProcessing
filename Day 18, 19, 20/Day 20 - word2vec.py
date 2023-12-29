91b0564bfc36040e35014bce255a46057a2529a3
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-12-29 20:38:53 +0545

    Gitignore

---
+++ Day 18, 19, 20/Day 20 - word2vec.py
@@ -0,0 +1,6 @@
+# Initializing the Word2vec Pretrained model:
+from gensim.models.keyedvectors import KeyedVectors
+# https://s3.amazonaws.com/d14j-distribution/GoogleNews-vectors-negative300.bin.gz
+# wget -c "https://s3.amazonaws.com/d14j-distribution/GoogleNews-vectors-negative300.bin.gz"
+# https://drive.google.com/u/0/uc?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM&export=download&resourcekey=0-wjGZdNAUop6WykTtMip30g
+
