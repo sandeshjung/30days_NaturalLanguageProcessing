910948b0ec1763630688d1c78ba7f5124b6b3aff
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2024-01-02 13:46:00 +0545

    Trained LSTM model

--- Day 22, 23/Day 23 - MoreLSTM.py
+++ Day 22, 23/Day 23 - MoreLSTM.py
@@ -36,3 +36,61 @@ for i, sentence in enumerate(sentences):
     for t, char in enumerate(sentence):
         X[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
+
+# Assemble a character-based LSTM model for generating text
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+optimizer = RMSprop(learning_rate=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.summary()
+
+
+epochs = 6
+batch_size = 128
+model_structure = model.to_json()
+from keras.callbacks import ModelCheckpoint
+checkpoint = ModelCheckpoint("best_weights.h5", monitor="val_loss", save_best_only=True, save_weights_only=True)
+with open("shakes_lstm_model.json", "w") as json_file:
+    json_file.write(model_structure)
+model.fit(X, y, batch_size=batch_size, epochs=epochs, callbacks=[checkpoint], validation_split=0.2)
+
+# Sampler to generate character sequence
+import random
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+# Generate threetexts with three diversity levels
+import sys
+start_index = random.randint(0, len(text) - maxlen - 1)
+for diversity in [0.2, 0.5, 1.0]:
+    print()
+    print('----- diversity:', diversity)
+    generated = ''
+    sentence = text[start_index: start_index + maxlen]
+    generated += sentence
+    print('---- Generating with seed: "' + sentence + '"')
+    sys.stdout.write(generated)
+    for i in range(400):
+        x = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence): #seed the trained network and see what it spits out as next character
+            x[0, t, char_indices[char]] = 1
+        preds = model.predict(x, verbose=0)[0] #model makes prediction
+        next_index = sample(preds, diversity)
+        next_char = indices_char[next_index] #look up which character that index represents
+        generated += next_char
+        sentnece = sentence[1:] + next_char # add it to the seed and drop the first character to keep the length the same
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+    print()
+    
